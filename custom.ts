aff23360638052c52b0bfa2fbab0a3bad90925cf
sen <sen.take119@gmail.com>
2025-01-15 17:27:24 +0900

    更新 pxt.json, main.blocks, main.ts, custom.ts

---
+++ custom.ts
@@ -0,0 +1,47 @@
+
+/**
+* このファイルを使って、独自の関数やブロックを定義してください。
+* 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="前"
+    前,
+    //% block="後ろ"
+    後ろ,
+    //% block="右"
+    右,
+    //% block="左"
+    左,
+    //% block="左"
+    止まる,
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#ffa500 icon="\uf075"
+namespace ロボコン {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: "速度"
+     * @param s describe parameter here, eg: "速度"
+     * @param e describe parameter here
+     */
+    //% block
+export function 速度(s: number , e: MyEnum): void {
+    if (true) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+}
